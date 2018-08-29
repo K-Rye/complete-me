b8d6a5dbb3b9d59a49e6af7abe1d99bca460cf8b
Kevin Ryan <kevinpatryan@gmail.com>
2018-08-29 16:44:58 -0600

    Insert function

--- test/trie-test.js
+++ test/trie-test.js
@@ -23,24 +23,26 @@ describe('trie', () => {
 
   it('should increase count each time we enter a new word', () => {
     expect(trie.count).to.eq(0);
-    trie.insert('james');
+    trie.insert('astounding');
     expect(trie.count).to.eq(1);
-    trie.insert('jason');
+    trie.insert('fantastic');
     expect(trie.count).to.eq(2);
-    trie.insert('jamming');
+    trie.insert('incredible');
     expect(trie.count).to.eq(3);
   });
 
   it ('should be able to insert multiple words', () => {
-    trie.insert ('james');
-    trie.insert ('jason');
-    trie.insert ('jamming');
-    trie.suggest('j')
-    // expect(Object.keys(trie.root.child)).to.deep.eq([ 'j' ]);
+    trie.insert ('astounding');
+    trie.insert ('fantastic');
+    trie.insert ('incredible');
+    trie.suggest('a')
+    trie.suggest('s')
+    expect(trie.suggest()). to equal()
+    expect(Object.keys(trie.root.child)).to.deep.eq([ 'a', 'f', 'i' ]);
     console.log(JSON.stringify(trie, null, 4));
-    console.log(expect(Object.keys(trie.root.child))
+    // console.log(expect(Object.keys(trie.root.child)))
   });
 
 
-// });
+});
 
